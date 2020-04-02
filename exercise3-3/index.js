1dbce35360f1afeb8fea135d9745136bc4edd629
banaanihillo <banaanihillo@protonmail.ch>
2020-04-02 12:10:18 +0300

    Commited exercise3-3 (Part 3 of Phonebook backend)
    Just added a comment indicating the existing functionality

---
+++ exercise3-3/index.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const app = express();
+app.use(express.json());
+
+let persons = [
+    {
+        id: Math.floor(Math.random() * 100000),
+        name: "Foo Bar",
+        number: "0-ONE",
+    },
+    {
+        id: Math.floor(Math.random() * 100000),
+        name: "Chocolate Bar",
+        number: "0-SUGAR",
+    }
+]
+
+app.get("/api/persons", (request, response) => {
+    response.json(persons)
+})
+
+// Already made this as part of exercise3-1, but apparently this is exercise3-3 now.
+app.get("/api/persons/:id", (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(person => person.id === id)
+    if (person) {
+        response.json(person)
+    } else {
+        response.status(404).end()
+    }
+})
+
+app.get("/info", (request, response) => {
+    const date = new Date()
+    response.send(`The phonebook currently has ${persons.length} entries.
+        This request was made on ${date}.`)
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+    console.log(`Running the server on port ${PORT}`)
+})
